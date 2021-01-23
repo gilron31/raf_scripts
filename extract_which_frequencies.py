1658ef4da15e547ba831e7706c3c1ec7a4b39316
Gilro <gilron31@gmail.com>
2021-01-23 22:10:23 +0200

    somwthing

--- extract_which_frequencies.py
+++ extract_which_frequencies.py
@@ -38,12 +38,15 @@ def get_f_129_estim_from_biglog(biglog_dict):
 def get_all_data_from_run(run_path, what_func, before_after = 0):
 	dirs_in_run = os.listdir(run_path)
 	res = []
+	counter = 1
 	for dir_in_run in dirs_in_run:
+		print("dealing with run: " + str(counter))
+		counter += 1
 		wps = os.listdir(os.path.join(run_path, dir_in_run,"WPS_before_and_after"))[before_after]
 		path_to_biglog = os.path.join(run_path, dir_in_run,"WPS_before_and_after", wps, BIGLOGMAT)
 		res.append(what_func(loadmat(path_to_biglog)))
 	return res
-	
+
 def main():
 	all_runs = os.listdir(path_to_real_recordings)
 	run1_highs = all_runs[0]
