c1091684049904c3a644ecb9d9ed2cafd2c940a0
Gilro <gilron31@gmail.com>
2021-01-23 18:40:45 +0200

    changed author

--- extract_which_frequencies.py
+++ extract_which_frequencies.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import numpy as np
 import scipy.io as spio
 
 print('hello world gil and his thesis')
+=======
+"import numpy as np" 
+"import scipy.io as spio" 
+>>>>>>> changed author
