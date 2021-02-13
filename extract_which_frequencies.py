00b35864e1b38f21906c4944bba03416803daaef
Gilro <gilron31@gmail.com>
2021-02-13 11:54:58 +0200

    added get G2 func and did tiding up

--- extract_which_frequencies.py
+++ extract_which_frequencies.py
@@ -32,9 +32,14 @@ BIGLOGMAT = 'bigLog.mat'
 LOGXFID = 'logXFID'
 F129 = 'f_129_estim'
 path_to_tempdir = "G:\\random_sampling_biglogs"
+LOGSXMAGS = "logSXMAGS"
 
 
 # %xconfig IPCompleter.use_jedi = False.
+def get_f_129_and_G2_from_biglog_path(path_to_biglog):
+	logsxmags = loadmat(path_to_biglog)[BIGLOG][LOGSXMAGS]
+	return logsxmags['f_0'], logsxmags['G2estim']
+
 def get_f_129_estim_from_biglog(path_to_biglog_dict):
 	return loadmat(path_to_biglog_dict)[BIGLOG][LOGXFID][F129]
 
@@ -74,35 +79,17 @@ def get_sensitivities_from_ress(resx, resy, driveamp, diode = 0):
 	#print("E_naive_x :" + str(E_naive_x))
 	#print("E_naive_y :" + str(E_naive_y))
 
-	#E_acc_both = ((Y_res_x ** 2 - Y_res_y ** 2)/(Y_alk_x ** 2 - Y_alk_y ** 2) - 1) * 0.5
-
 	a = Y_alk_x ** 2 + Y_alk_y ** 2 
-	#det_x = Y_alk_x ** 4 - a * (Y_alk_x ** 2 - Y_res_x ** 2) 
-	#E_acc_x = (- Y_alk_x ** 2 + np.sqrt(det_x)) / a
-	#E_acc_y = (- Y_alk_y ** 2 + np.sqrt(det_y)) / a
-	#E_acc_ym = (- Y_alk_y ** 2 - np.sqrt(det_y)) / a
-
-	E_solid_x = Y_res_x / np.sqrt(a) - 1
-	E_solid_y = Y_res_y / np.sqrt(a) - 1
-
 	alpha = (Y_res_x ** 2 + Y_res_y ** 2) / a
-	E_ver2p = -0.5 + np.sqrt(0.25 - 0.5 * (1 - alpha)) 
-	E_ver2m = -0.5 - np.sqrt(0.25 - 0.5 * (1 - alpha)) 
-	print("E_ver2p: " + str(E_ver2p))
-	print("E_ver2m: " + str(E_ver2m))
+
+	#E_lb_x = Y_res_x / np.sqrt(a) - 1
+	#E_lb_y = Y_res_y / np.sqrt(a) - 1
+
 
 	E_ver3p =  np.sqrt( - 0.5 * (1 - alpha)) 
-	print("E_ver3p: " + str(E_ver3p))
-	#print("E_acc_x: " + str(E_acc_x))
-#	print("E_acc_xm: " + str(E_acc_xm))
-#	print("E_acc_y: " + str(E_acc_y))
-	#print("E_acc_ym: " + str(E_acc_ym))
-	print("E_solid_x: " + str(E_solid_x))
-	print("E_solid_y: " + str(E_solid_y))
-	print("E_solid_xp1: " + str(E_solid_x + 1))
-	print("E_solid_yp1: " + str(E_solid_y + 1))
+	#print("E_ver3p: " + str(E_ver3p))
 
-	Xe_amp = max(E_solid_x, E_solid_y) * np.sqrt(a)
+	Xe_amp = E_ver3p * np.sqrt(a)
 	#print("Xe_amp " + str(Xe_amp))
 	return Y_alk_x, Y_alk_y, E_ver3p 
 
@@ -116,6 +103,9 @@ def get_ress_from_biglog_obj(biglog):
 	resy = logSXMAGS["res_fory"]
 	driveamp = logSXMAGS["driveamp_G"]	
 	return resx, resy, driveamp
+
+def get_sensitivities_from_biglog_path(biglog_path, diode = 0):
+	return get_sensitivities_from_biglog_obj(loadmat(biglog_path), diode)
 
 def get_all_data_from_run(run_path, what_func, before_after = 0):
 	dirs_in_run = os.listdir(run_path)
