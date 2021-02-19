4eeb641f02fa301d6ff417398a28887ba2be0776
Gilro <gilron31@gmail.com>
2021-02-19 19:22:05 +0200

    change

--- extract_which_frequencies.py
+++ extract_which_frequencies.py
@@ -37,8 +37,8 @@ LOGSXMAGS = "logSXMAGS"
 def get_biglogs_from_run(p1):
 	list_of_biglogs = []
 	for p in p1:
-		ptemp1 = os.listdir(os.join(p1, p, 'WPS_before_and_after'))[0]
-		ptemp2 = os.join(p1, p, 'WPS_before_and_after', ptemp1, 'bigLog.mat')
+		ptemp1 = os.listdir(os.path.join(p1, p, 'WPS_before_and_after'))[0]
+		ptemp2 = os.path.join(p1, p, 'WPS_before_and_after', ptemp1, 'bigLog.mat')
 		list_of_biglogs.append(ptemp2)
 	return list_of_biglogs
 
